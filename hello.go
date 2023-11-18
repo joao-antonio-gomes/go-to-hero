e611ce07f5456a6501bdcc592693993bffce5f58
João Antônio Gomes <devjoaoantoniogomes@gmail.com>
2023-11-18 07:46:47 -0300

    estrutura de controle com go: if e switch

--- hello.go
+++ hello.go
@@ -16,6 +16,16 @@ func main() {
 	var comando int
 	fmt.Scan(&comando)
 
-	fmt.Println("O endereço da variável comando é", &comando)
-	fmt.Println(comando)
+	fmt.Println("O comando escolhido foi", &comando)
+
+	switch comando {
+	case 1:
+		fmt.Println("Iniciando monitoramento...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Comando não reconhecido...")
+	}
 }
